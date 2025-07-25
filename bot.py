28f4e51cf086b3fcb616d25479ce0a516b0dd141
merkulova-mar <markulova.mer@gmail.com>
2025-07-25 20:03:12 +0700

    Update bot.py_3

--- bot.py
+++ bot.py
@@ -4,13 +4,14 @@ import pymorphy2
 import asyncio
 import os
 from aiogram.filters import Command, Filter
+import random
 
 # Получаем токен из переменной окружения
 API_TOKEN = os.getenv("API_TOKEN")
 
 # Словарь сотрудников и их ключевых слов (будут нормализованы)
 KEYWORDS = {
-    "ксения": ["flow", "английский", "upskill", "сквозная английского", "funnel by channel", "маркетинг английский"],
+    "ксения": ["flow", "английский", "upskill", "сквозная английского", "funnel by channel", "маркетинг английский","эксперимент"],
     "евгения": ["открутки", "reskill маркетинг"],
     "канат": ["английский", "тьюторы английский", "финансы английский"],
     "инесса": ["b2b"],
@@ -20,7 +21,7 @@ KEYWORDS = {
         "маркетинговая аналитика", "канал привлечения", "трафик", "конверсия", "фритрек",
         "конверсия в регистрацию", "рекламные кампании", "cpa", "cpc", "cpm", "roi маркетинга",
         "атрибуция", "первый клик", "последний клик", "cac", "эффективность креативов",
-        "воронка привлечения", "attribution", "атрибуция", "аквизитион", "acquisition", "acqusition", "aquisation", "acquisiton", "acqisition", "acquisitionn", "acquisishon", "acqusishon", "acquizition", "acquesition", "acquisicion", "acquisitin", "acquisishan", "acquisation", "acquisitian","аквизишн", "аквузишн", "акуизейшн", "аквизитон", "аквизишн", "аквизишнн", "аквизишон", "аквузишон", "аквизишн", "аквезишн", "аквизишн", "аквизитин", "аквизишан", "аквизейшн", "аквизитиан"
+        "воронка привлечения", "attribution", "атрибуция", "аквизитион", "acquisition", "acqusition", "aquisation", "acquisiton", "acqisition", "acquisitionn", "acquisishon", "acqusishon", "acquizition", "acquesition", "acquisicion", "acquisitin", "acquisishan", "acquisation", "acquisitian","аквизишн", "аквузишн", "акуизейшн", "аквизитон", "аквизишн", "аквизишнн", "аквизишон", "аквузишон", "аквизишн", "аквезишн", "аквизишн", "аквизитин", "аквизишан", "аквизейшн", "аквизитиан", "эксперимент"
     ],
     "пользовательский опыт": ["трудоустройство", "доходимость", "спринты"],
     "bi и моделирования": [
@@ -47,7 +48,9 @@ PHOTOS = {
     "дмитрий": "AgACAgIAAxkBAAMdaIJFJRPLodBKt8ttOklnsjpK4KUAAqH0MRsOLRhIb0u6Yj7h5GYBAAMCAAN4AAM2BA",
     "инесса": "AgACAgIAAxkBAAMfaIJFMZFDMiLJ3ubZJzg7SOUjoTAAAqL0MRsOLRhIkKU_j7KCWjABAAMCAAN4AAM2BA",
     "изабелла": "AgACAgIAAxkBAAMhaIJFQTXIQjp1PnbSYJ8t9fQDM0oAAqP0MRsOLRhI3X5T5vgm_X0BAAMCAAN4AAM2BA",
-    "канат": "AgACAgIAAxkBAAMLaIJAY25KLoByqupVgS8mCBra6lMAAhf0MRusUhFIYddKCGwWd80BAAMCAAN4AAM2BA"
+    "канат": "AgACAgIAAxkBAAMLaIJAY25KLoByqupVgS8mCBra6lMAAhf0MRusUhFIYddKCGwWd80BAAMCAAN4AAM2BA",
+    "привлечение": "AgACAgIAAxkBAAOHaIN5W1jgZxHllhg0TL4ELUpQLFEAAlv0MRsqBSBIw6MyTWZ9b2IBAAMCAAN5AAM2BA",
+    "bi и моделирования": "AgACAgIAAxkBAAOSaIN9cuZvzub40VNGCY5qAoI1OB4AAnT0MRsqBSBI1197h6zuWf0BAAMCAAN5AAM2BA"
 }
 
 links = {
@@ -88,6 +91,14 @@ NOT_ANALYTICS_KEYWORDS = [
     "договор", "счет", "оплата", "касса", "банк", "карта", "парковка", "охрана", "уборщица", "сантехник"
 ]
 
+NOT_ANALYTICS_PHOTOS = [
+    "AgACAgIAAxkBAAOUaIN-wN4lEqiL5gk-5e2Fl6bPgiAAAnz0MRsqBSBIIg1UJOGNevwBAAMCAAN4AAM2BA",
+    "AgACAgIAAxkBAAOWaIN_MLQvI5Ca0JCGaAQuBOjqch8AAmL5MRsLkhlI4MqVwEeNR5wBAAMCAAN4AAM2BA",
+    "AgACAgIAAxkBAAOYaIN_SE1nvaIEV7Ke8h2MfzXyqckAAoP0MRsqBSBIFbDUaWb6LakBAAMCAAN4AAM2BA",
+    "AgACAgIAAxkBAAOaaIN_UhTzf140W72Pab4AAQHSoY3IAAKE9DEbKgUgSDvl_RWOztaPAQADAgADbQADNgQ",
+    "AgACAgIAAxkBAAOcaIN_okaFw9uL-61ShaIuR2zs3wsAAof0MRsqBSBIC88LiSU0LMcBAAMCAAN4AAM2BA"
+]
+
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
@@ -111,7 +122,8 @@ async def find_expert(message: types.Message):
     for phrase in NOT_ANALYTICS_KEYWORDS:
         phrase_words = [normalize_word(w) for w in re.findall(r'\w+', phrase.lower(), re.UNICODE)]
         if all(w in norm_words for w in phrase_words):
-            await message.answer("Наши аналитики великолепны, но не всемогущи. Возможно вам поможет разработка")
+            photo_id = random.choice(NOT_ANALYTICS_PHOTOS)
+            await message.answer_photo(photo_id, caption="Наши аналитики великолепны, но не всемогущи. Возможно вам поможет разработка")
             return
 
     analytic_depts = ["привлечение", "пользовательский опыт", "bi и моделирования"]
@@ -137,7 +149,10 @@ async def find_expert(message: types.Message):
                 person = person_key
                 break
     if not person:
-        await message.answer("Я не нашёл подходящего специалиста. Попробуй переформулировать вопрос или спроси в канале https://mattermost.practicum.yandex/practicum/channels/analytics_communications.")
+        await message.answer_photo(
+            "AgACAgIAAxkBAAOeaIOAGDKIjkAgZoweLFXPLQKWCtgAAnj5MRvKQiBI6iilAUx2Rp4BAAMCAANtAAM2BA",
+            caption="Я не нашёл подходящего специалиста. Попробуй переформулировать вопрос или спроси в канале https://mattermost.practicum.yandex/practicum/channels/analytics_communications."
+        )
     else:
         link = links.get(person)
         caption = f"{person.title()} ({MENTIONS.get(person, person)}) может помочь тебе!"
